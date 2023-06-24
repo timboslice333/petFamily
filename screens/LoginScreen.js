f4e559c9d90eb881e2cefd54ab06b8aad00bf8b0
Chris <jiachenlu1211@gmail.com>
2023-06-24 11:53:38 -0700

    Chris

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,22 +1,10 @@
-import React from "react";
-import { Text, View, TouchableOpacity, StyleSheet, Image} from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 
-export const SignupScreen = ({ navigation }) => {
+export const LoginScreen = ({ navigation }) => {
   return (
     <>
       <View style={{ marginTop: 100 }}>
-        <Text style={[styles.header_label]}>
-          PetSOS
-        </Text>
-        {/* <Image source={require('../assets/Login.jpg')} style={styles.image} />bbb */}
-        <TouchableOpacity style={[styles.button]}>
-          <Text style={styles.buttonText}>User Sign Up</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>User Login</Text>
-        </TouchableOpacity>
-        
-        <Text>SignupScreen</Text>
+        <Text>LoginScreen</Text>
         <TouchableOpacity
           style={styles.button}
           onPress={() => navigation.pop()}
@@ -35,17 +23,6 @@ export const SignupScreen = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  header_label: {
-    fontSize: 36,
-    marginTop: 40,
-    marginLeft: 0,
-  },
-  image: {
-    width: 973,
-    height: 1163,
-    justifyContent: "center",
-    alignItems: "center",
-  },
   button: {
     marginVertical: 10,
     borderRadius: 25,
