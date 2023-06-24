02ac13bf827fc61565fd2a33efd9932b63ddd82f
Timothy Ma <timothy.ma@sap.com>
2023-06-23 22:28:39 -0700

    removed gitIgnor for aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,26 @@
+const awsmobile = {
+    "aws_project_region": "us-west-2",
+    "aws_cognito_identity_pool_id": "us-west-2:8b54f64d-caf4-4217-b7c0-e93533da0c4a",
+    "aws_cognito_region": "us-west-2",
+    "aws_user_pools_id": "us-west-2_Zj3jASizB",
+    "aws_user_pools_web_client_id": "mokfb12ntisj6oafber0re8ps",
+    "oauth": {},
+    "aws_cognito_username_attributes": [],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+export default awsmobile;
