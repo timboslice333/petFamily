77a11405271f2eed2da04705835e7c8fb5dab2ad
Haoyuan Chen <chenhaoyuan1017@gmail.com>
2023-06-23 23:15:32 -0700

    color constants

---
+++ colors.js
@@ -0,0 +1,9 @@
+const Colours = {
+  primary: '#FDBD4E', //the orange；
+  primary_variant: '#2D384C', // the dark blue
+  secondary: '#FED795', // the lighter orange
+  
+  
+};
+
+export default Colours;
