e3d75e588926fa36b82f1f9671613ec81b31edf9
Haoyuan Chen <chenhaoyuan1017@gmail.com>
2023-06-25 00:04:07 -0700

    done

--- screens/PostsScreen.js
+++ screens/PostsScreen.js
@@ -1,10 +1,11 @@
-import { Text, View, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Text, View, TouchableOpacity, Image,ImageBackground, StyleSheet } from "react-native";
 import { useRoute } from '@react-navigation/native';
 import Colours from "../constants/colors";
 
 export const PostsScreen = ({ route,navigation }) => {
   
   const { petName, lastSeenLocation, notes } = route.params;
+  
     //const petName = 'Code';
     //const lastSeenLocation=  'City Park';
     //const notes = 'Friendly and approachable';
@@ -12,6 +13,8 @@ export const PostsScreen = ({ route,navigation }) => {
   
     return (
       <View style={styles.container}>
+     
+      
         <View style={styles.header}>
           <Text style={styles.headerText}>Your Post</Text>
         </View>
@@ -21,21 +24,28 @@ export const PostsScreen = ({ route,navigation }) => {
         
       />
         <View style={styles.postDateTimeSection}>
-          <Text style={styles.label}>Post Date and Time</Text>
-          <Text>{postDateTime}</Text>
+          <Text style={styles.time}>{postDateTime}</Text>
         </View>
         <View style={styles.photoSection}>
           {/* Add your photo component here */}
         </View>
         <View style={styles.textEntryContainer}>
-           <Text>{petName}</Text> 
-          <Text>{lastSeenLocation}</Text> 
-          <Text>{notes}</Text>
-       </View>
+        <Text style={styles.label}>Pet Name : </Text>
+        <Text style={styles.text}>{petName}</Text>
+        <Text style={styles.label}>Last Seen Location: </Text>
+        <Text style={styles.text}>{lastSeenLocation}</Text>
+        <Text style={styles.label}>Notes: </Text>
+        <Text style={styles.text}>{notes}</Text>
         <View style={styles.tagsSection}>
-          <Text style={styles.label}>Tags</Text>
+          <Text style={styles.label}>Tags : Missing</Text>
           {/* Add your tags component (Lost/Found) here */}
         </View>
+       </View>
+       <Image
+        source={require('/Users/timothyma/petSOS/assets/pets.png')}
+        style={styles.bottomPhoto}
+        
+      />
       </View>
     );
   };
@@ -43,23 +53,40 @@ export const PostsScreen = ({ route,navigation }) => {
   
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "stretch",
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#f9f9f9',
+    backgroundColor: Colours.primary,
     padding: 20,
   },
   header: {
-    alignItems: 'center',
+    alignSelf: 'center',
     marginBottom: 20,
-    marginTop:50,
+    position: "absolute",
+    marginTop:60
   },
   photo: {
     alignSelf: 'center',
-    width: 350,
-    height: 350,
+    width: 300,
+    height: 300,
     borderWidth: 10,
+    borderRadius:30,
     borderColor: Colours.primary_variant,
+    position: "absolute",
+    marginTop:120
+  
     
+  },
+  text:{
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginLeft:30,
+    color: Colours.primary_variant
   },
   headerText: {
     fontSize: 20,
@@ -67,24 +94,32 @@ const styles = StyleSheet.create({
   },
   postDateTimeSection: {
     marginBottom: 20,
+    alignSelf: 'center',
+    marginTop:440
   },
   photoSection: {
     alignItems: 'center',
     marginBottom: 20,
   },
   textEntryContainer: {
-    marginBottom: 20,
+    marginBottom: 30,
   },
   label: {
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 10,
+    marginLeft:30
+  },
+  time: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
   textInput: {
-    borderWidth: 1,
+    borderWidth: 6,
     borderColor: '#ccc',
     borderRadius: 8,
-    paddingVertical: 10,
+    paddingVertical: 20,
     paddingHorizontal: 16,
     fontSize: 16,
     backgroundColor: 'white',
@@ -94,6 +129,9 @@ const styles = StyleSheet.create({
     textAlignVertical: 'top',
   },
   tagsSection: {
-    marginBottom: 20,
+    marginBottom: 50,
+  },
+  bottomPhoto: {
+    
   },
 });
