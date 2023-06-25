4070ea69624d8fb17038e5e528d7340d2b7d5a0f
Chris <jiachenlu1211@gmail.com>
2023-06-24 17:48:44 -0700

    chris

--- screens/DiscoverScreen.js
+++ screens/DiscoverScreen.js
@@ -6,9 +6,13 @@ export const DiscoverScreen = ({ navigation }) => {
     <>
       <View>
       <View style={styles.banner} />
-        <TouchableOpacity style={styles.picture_1}>  
+        <TouchableOpacity style={styles.picture_1}>
+        <Image source={require('../assets/Disc_Pic.jpeg')} style={styles.picture_2}></Image>
         </TouchableOpacity>
-        <TouchableOpacity
+        <View style={styles.rectangle} />
+        <Text style={styles.rect_text}>Found my dog Using PetSOS!</Text>
+        <Text style={styles.timer_text}>12 minutes</Text>
+        {/* <TouchableOpacity
           style={styles.button}
           onPress={() => navigation.pop()}
         >
@@ -20,7 +24,7 @@ export const DiscoverScreen = ({ navigation }) => {
         >
           <Text style={styles.buttonText}>go to posts screen</Text>
         </TouchableOpacity>
-        <Text>DiscoverScreen</Text>
+        <Text>DiscoverScreen</Text> */}
       </View>
     </>
   );
@@ -29,6 +33,8 @@ export const DiscoverScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   picture_1: {
     borderRadius: 10,
@@ -36,6 +42,40 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 100,
     left: 150,
+  },
+  picture_2: {
+    borderRadius: 10,
+    alignContent: 'center',
+    position: 'absolute',
+    top: -20,
+    left: -135,
+    width: 400,
+    height: 250,
+  },
+  rectangle: {
+    backgroundColor: '#FDBD4E',
+    position: 'absolute',
+    borderRadius: 10,
+    top: 280,
+    left: 15,
+    width: 400,
+    height: 150,
+  },
+  rect_text: {
+    fontSize: 36,
+    fontWeight: "600",
+    color: "#2D384C",
+    position: 'absolute',
+    top: 300,
+    left: 40,
+  },
+  timer_text: {
+    fontsize: 16,
+    fontWeight: '400',
+    color: '#2D384C',
+    position: 'absolute',
+    top: 395,
+    left: 40,
   },
   banner: {
     backgroundColor: Colours.primary,
